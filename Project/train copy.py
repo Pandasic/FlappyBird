b0a738256cb24342437d4654241392cd2f67e50f
TB521 <37146341+TB521@users.noreply.github.com>
2020-06-30 14:44:13 +0800

    model

--- Project/train copy.py
+++ Project/train copy.py
@@ -1,3 +1,4 @@
+#该文件为继续训练后的修改文件
 import parl
 from parl import layers
 import paddle.fluid as fluid
@@ -15,12 +16,11 @@ from ReplayMemory import ReplayMemory
 from ple.games.flappybird import FlappyBird
 from ple.ple import PLE
 
-import pygame.display
 LEARN_FREQ = 10 # 训练频率，不需要每一个step都learn，攒一些新增经验后再learn，提高效率
 MEMORY_SIZE = 2000    # replay memory的大小，越大越占用内存
 MEMORY_WARMUP_SIZE = 1000  # replay_memory 里需要预存一些经验数据，再开启训练
 BATCH_SIZE = 64   # 每次给agent learn的数据数量，从replay memory随机里sample一批数据出来
-LEARNING_RATE = 0.0001 # 学习率
+LEARNING_RATE = 0.001 # 学习率
 GAMMA = 0.99 # reward 的衰减因子，一般取 0.9 到 0.999 不等
 
 import os
@@ -45,15 +45,8 @@ def remapAction(act):
     if act == 1:
         return 119
 
-episode = 8200
-eval_time = 0
-
-maxScore = 0
-maxScoreEp = episode
-
 # 训练一个episode
 def run_episode(env, agent, rpm):
-    global maxScore,maxScoreEp
     total_reward = 0
     env.reset_game()
     obs = remapGameState(env.getGameState())
@@ -75,11 +68,6 @@ def run_episode(env, agent, rpm):
                                      batch_next_obs,
                                      batch_done)  # s,a,r,s',done
 
-        pygame.display.set_caption("score:%d,maxScore:%d,EP = %d"%(int(env.score()),maxScore,maxScoreEp))
-        if env.score() >maxScore:
-            maxScore = env.score()
-            maxScoreEp = episode
-
         total_reward += reward
         obs = next_obs
         if done:
@@ -88,7 +76,6 @@ def run_episode(env, agent, rpm):
 
 # 评估 agent, 跑 5 个episode，总reward求平均
 def evaluate(env, agent, render=False):
-    global maxScore,maxScoreEp
     eval_reward = []
     for i in range(5):
         env.reset_game()
@@ -105,10 +92,6 @@ def evaluate(env, agent, render=False):
             episode_reward += reward
             if done:
                 break
-            pygame.display.set_caption("score:%d,maxScore:%d,EP = %d"%(int(env.score()),maxScore,maxScoreEp))
-            if env.score() >maxScore:
-                maxScore = env.score()
-                maxScoreEp = episode
 
         eval_reward.append(episode_reward)
     return np.mean(eval_reward)
@@ -130,7 +113,7 @@ agent = Agent(
     algorithm,
     obs_dim=obs_shape[0],
     act_dim=action_dim,
-    #e_greed=0.5,  # 有一定概率随机选取动作，探索
+    #e_greed=0.01,  # 有一定概率随机选取动作，探索
     e_greed=0.05,  # 有一定概率随机选取动作，探索
     e_greed_decrement=10e-7)  # 随着训练逐步收敛，探索的程度慢慢降低
 
@@ -148,7 +131,8 @@ while len(rpm) < MEMORY_WARMUP_SIZE:
 max_episode = 50000
 
 # 开始训练
-
+episode = 0
+eval_time = 0
 
 while episode < max_episode:  # 训练max_episode个回合，test部分不计算入episode数量
     # train part
